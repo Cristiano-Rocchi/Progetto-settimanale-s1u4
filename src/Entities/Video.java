4188f2819d7ee862914332b4ca925f552ae80bae
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-07-26 14:23:29 +0200

    new

--- src/Entities/Video.java
+++ src/Entities/Video.java
@@ -1,6 +1,47 @@
 package Entities;
 
-public class Video {
+import Interfaces.Luminosita;
+import Interfaces.Play;
+
+
+public class Video extends ElementoMultimediale implements Play, Luminosita {
+    private int luminosita;
+    private int durata;
+    private int volume;
+
+    public Video(String titolo, int luminosita, int durata, int volume) {
+        super(titolo);
+    }
+
+    @Override
+    public void play() {
+        /*String volume = "";
+
+        for (int i = 0; i < this.volume; i++) {
+            volume += "!";
+        }
+        for (int i = 0; i < this.durata; i++) {
+            System.out.println(getTitolo() + volume);
+
+        }*/
+        for (int i = 0; i < durata; i++) {
+            System.out.println(getTitolo() + " " + "!".repeat(volume));
+        }
+
+        System.out.println("ciao");
+    }
+
+    @Override
+    public void luminosita() {
+
+    }
+
+
+    /*COSTRUTTORI*/
+
+
+
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import Entities.RegistrazioneAudio;
+import Entities.Video;
+
 public class Main {
     public static void main(String[] args) {
-        /* RegistrazioneAudio registrazioneAudio = new RegistrazioneAudio("Audio1", 1, 15);*/
-
-
+        RegistrazioneAudio registrazioneAudio = new RegistrazioneAudio("Audio1", 1, 15);
+        Video video = new Video("video1", 5, 10, 7);
+        
+        video.play();
     }
 }
